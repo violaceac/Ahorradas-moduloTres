eb2512b96bcbac8683688cce7c5e486c074bf828
Violeta Contreras <viol.contreras@gmail.com>
2025-02-13 15:59:20 -0300

    cambios en las vistas y tailwind a vista balance

---
+++ index.js
@@ -0,0 +1,28 @@
+
+// "CATEGORIA CON MAYOR GANANCIA"
+// REDUCE PARA CADA CATEGORIA PARA OBTENER UN TOTAL Y HACER COMPARACION
+
+// "CATEGORIA CON MAYOR GASTO"
+// REDUCE PARA CADA CATEGORIA PARA OBTENER UN TOTAL Y HACER COMPARACION
+
+
+
+
+
+
+
+
+
+
+
+
+// CLASES 
+
+// $modoOscuro.addEventListener("click", () => {
+//     if($body.classList.contains("modo-claro")) {
+//         $body.classList.remove("modo-claro")
+//         $body.classList.add("modo-oscuro");
+//         $modoOscuro.style.display = "none";
+//         $modoClaro.style.display = "flex"
+//     }
+// })
