4bce6d30e202157821329b35804ca4de48cde92e
Violeta Contreras <viol.contreras@gmail.com>
2025-02-16 20:08:56 -0300

    prueba para mergear

--- index.js
+++ index.js
@@ -2,18 +2,45 @@
 // "CATEGORIA CON MAYOR GANANCIA"
 // REDUCE PARA CADA CATEGORIA PARA OBTENER UN TOTAL Y HACER COMPARACION
 
-// "CATEGORIA CON MAYOR GASTO"
-// REDUCE PARA CADA CATEGORIA PARA OBTENER UN TOTAL Y HACER COMPARACION
+
+// selector de clases
+function $$(selector) {
+    return document.querySelectorAll(selector);
+  }
+
+// selector de id
+function $(selector) {
+    return document.querySelector(selector);
+  }
+
+//boton para desplegar menu hamburguesa
+let $botonMenu = $("#boton-hamburguesa")
+
+// contenedor botones 
+let $modalBotones = $("#botones-hamburguesa")
+
+// botones para vistas
+let $btnVistaBalance = $$(".balance");
+let $btnVistaCategorias = $$(".categorias");
+let $btnVistaReportes = $$(".reportes")
 
 
 
+// EVENTO CLICK menu hamburguesa
+$botonMenu.addEventListener("click", () => {
+    $modalBotones.classList.toggle("hidden")
+})
 
-
-
-
-
-
-
+// FORMA LARGA MENU HAMBURGUESA
+// $botonMenu.addEventListener("click", () => {
+//     if ($modalBotones.classList.contains("hidden")) {
+//         $modalBotones.classList.remove("hidden")
+//         $modalBotones.classList.add("flex")
+//     } else if($modalBotones.classList.contains("flex")) {
+//         $modalBotones.classList.remove("flex")
+//         $modalBotones.classList.add("hidden")
+//     }
+// })
 
 
 // CLASES 
