7e2340865d9f25ab5dd65cbbb43be2536c1778f7
Violeta Contreras <viol.contreras@gmail.com>
2025-02-19 21:53:10 -0300

    botones ocultar y mostrar vistas, solo menu hamburguesa

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+window.onload = () => {
 
 // "CATEGORIA CON MAYOR GANANCIA"
 // REDUCE PARA CADA CATEGORIA PARA OBTENER UN TOTAL Y HACER COMPARACION
@@ -5,32 +6,29 @@
 
 // selector de clases
 function $$(selector) {
-    return document.querySelectorAll(selector);
-  }
+  return document.querySelectorAll(selector);
+}
 
 // selector de id
 function $(selector) {
-    return document.querySelector(selector);
-  }
+  return document.querySelector(selector);
+}
+
+// ========================================
 
 //boton para desplegar menu hamburguesa
 let $botonMenu = $("#boton-hamburguesa")
-
-// contenedor botones 
-
+// contenedor botones menu hamburguesa
 let $modalBotones = $("#botones-hamburguesa")
 
-// botones para vistas
-let $btnVistaBalance = $$(".balance");
-let $btnVistaCategorias = $$(".categorias");
-let $btnVistaReportes = $$(".reportes")
 
-
+// =========================================
 
 // EVENTO CLICK menu hamburguesa
 $botonMenu.addEventListener("click", () => {
-    $modalBotones.classList.toggle("hidden")
+  $modalBotones.classList.toggle("hidden")
 })
+
 
 // FORMA LARGA MENU HAMBURGUESA
 // $botonMenu.addEventListener("click", () => {
@@ -44,6 +42,108 @@ $botonMenu.addEventListener("click", () => {
 // })
 
 
+// ===========================================
+
+// VISTAS
+
+// secciones
+let $sectionVistaBalance = $("#vista-balance");
+let $sectionVistaCategorias = $("#vista-categorias");
+let $sectionVistaReportes = $("#vista-reportes");
+
+// botones para vistas
+let $btnVistaBalance = $("#balance");
+let $btnVistaCategorias = $("#categorias");
+let $btnVistaReportes = $("#reportes")
+
+// cambiar vistas
+
+// Ocultar un elemento
+$btnVistaCategorias.addEventListener("click", () => {
+$sectionVistaCategorias.style.display = "flex";
+$sectionVistaReportes.style.display = "none";
+$sectionVistaBalance.style.display = "none";
+});
+
+$btnVistaBalance.addEventListener("click", () => {
+  $sectionVistaBalance.style.display = "flex";
+  $sectionVistaReportes.style.display = "none";
+  $sectionVistaCategorias.style.display = "none";
+});
+
+$btnVistaReportes.addEventListener("click", () => {
+  $sectionVistaReportes.style.display = "flex";
+  $sectionVistaCategorias.style.display = "none";
+  $sectionVistaBalance.style.display = "none";
+});
+
+
+
+// element.style.display = "none";  // JavaScript puro
+// $("#elemento").hide();           // jQuery
+
+// // Mostrar un elemento 
+// element.style.display = "block"; // JavaScript puro
+// $("#elemento").show();
+
+
+
+
+
+
+
+// $btnVistaBalance.addEventListener("click", () => {
+//   showElement([$sectionVistaBalance]);
+//   hideElement([$sectionVistaCategorias, $sectionVistaReportes])
+// })
+
+// $btnVistaCategorias.addEventListener("click", () => {
+//   showElement([$sectionVistaCategorias]);
+//   hideElement([$sectionVistaBalance, $sectionVistaReportes])
+// })
+
+// $btnVistaReportes.addEventListener("click", () => {
+//   showElement([$sectionVistaReportes]);
+//   hideElement([$sectionVistaCategorias, $sectionVistaBalance])
+// })
+// $("#button-view-home").addEventListener("click", () => {
+//   showElement([$sectionViewHome])
+//   hideElement([$sectionViewVenta, $sectionViewReporte])
+// })
+
+
+// NUEVA OPERACION
+
+//boton nueva operacion
+let $botonNuevaOp = $$(".boton-nueva-op");
+
+//cards vistas
+let $cardSinOp = $("#card-sin-operaciones");
+let $cardNuevaOp = $("#vista-nueva-op");
+let $cardOpActivas = $("#operaciones-activas")
+
+// $botonNuevaOp.addEventListener("click", () => {
+// $cardNuevaOp.classList.toggle("hidden")
+// })
+
+
+// $("#button-view-home").addEventListener("click", () => {
+//   showElement([$sectionViewHome])
+//   hideElement([$sectionViewVenta, $sectionViewReporte])
+// })
+
+// cuando hago click en el boton nueva operacion pasan las siguientes cosas
+// + se abre la vista nueva operacion con el formulario para rellenar la operacion
+// + se oculta la vista balance o la vista operaciones activas
+
+
+
+
+
+
+
+
+
 // CLASES 
 
 // $modoOscuro.addEventListener("click", () => {
@@ -54,6 +154,10 @@ $botonMenu.addEventListener("click", () => {
 //         $modoClaro.style.display = "flex"
 //     }
 // })
+
+
+
+}
 
 
 
