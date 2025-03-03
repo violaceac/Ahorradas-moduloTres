fd8f1702973a89d606f22eea008045300720cf3d
Violeta Contreras <viol.contreras@gmail.com>
2025-03-03 16:15:34 -0300

    funcionalidades sectionBalance

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function ocultarElemento(selectors) {
 $btnVistaBalance.forEach(e => {
   e.addEventListener("click", () => {
     mostrarElemento([$sectionVistaBalance]);
-    ocultarElemento([$sectionVistaReportes, $sectionVistaCategorias, $sectionNuevaOp, $modalBotones])
+    ocultarElemento([$sectionVistaReportes, $sectionVistaCategorias, $sectionNuevaOp, $modalBotones, $sectionEditarOp])
     });
 })
 $btnVistaCategorias.forEach(e => {
@@ -80,7 +80,9 @@ $btnVistaReportes.forEach(e => {
 })
 
 
-// ===== nueva operacion ============================================
+// ===== VISTA BALANCE ============================================
+/////
+// ===== nueva operacion ===================================
 
 //array vacio para guardar operaciones
 let todasLasOp = []
@@ -93,12 +95,12 @@ let $sectionNuevaOp = $("#nueva-op");
 let $cardSinOp = $("#card-sin-operaciones")
 let $vistaOpActivas = $("#card-operaciones-activas")
 let $formAgregar = $("#form-agregar")
-let $formEditar = $("#form-editar")
 
 // evento click boton nuevaOp
 $botonNuevaOp.forEach(e => {
   e.addEventListener("click", () => {
-    mostrarElemento([$sectionNuevaOp]);
+    $formAgregar.reset();
+    mostrarElemento([$sectionNuevaOp, $formAgregar]);
     ocultarElemento([$sectionVistaBalance]);
     });
 })
@@ -119,7 +121,15 @@ $formAgregar.addEventListener("submit", (evento) => {
   todasLasOp.push(nuevaOp)
   guardarEnLS("operaciones", todasLasOp)
   pintarOperaciones(todasLasOp)
+  hayOp()
+  actualizarBalance()
    
+})
+
+//agregar y volver a "vistaBalance"
+$botonAgregar.addEventListener("click", () => {
+  mostrarElemento([$sectionVistaBalance, $vistaOpActivas]);
+  ocultarElemento([$sectionNuevaOp, $cardSinOp])
 })
 
 // mostrar operaciones
@@ -143,111 +153,227 @@ function pintarOperaciones(array) {
   agregarEventosEditYDelete()
 }
 
-///////// Prueba botón eliminar OK
+//funcion para mostrar las operaciones si hay cargadas
+function hayOp() {
+  if($tableOperaciones.firstChild) {
+    mostrarElemento([$vistaOpActivas])
+    ocultarElemento([$cardSinOp])
+    
+  } else {
+    mostrarElemento([$cardSinOp])
+    ocultarElemento([$vistaOpActivas])
+  }
+}
 
+// ===== editar o eliminar operacion =======================
+/////
+//captura vista formulario editar
+let $sectionEditarOp = $("#editar-op");
+//captura formulario editar
+let $formEditar = $(".form-editar");
+
+// funcion eliminar operacion
 function quitarOperacion(idOperacion) {
   const datos = leerLS("operaciones")
   const nuevoArray = datos.filter(operacion => operacion.id !== idOperacion)
 
-  guardarEnLS("operaciones", nuevoArray)
+  todasLasOp = nuevoArray; 
 
-  return nuevoArray
+  guardarEnLS("operaciones", todasLasOp);
+
+  return todasLasOp;
 } 
 
-////////
+// funcion para editar operacion
+function editarOperacion(idOperacion, nuevosDatos) {
+  const datos = leerLS("operaciones")
+  const indiceBuscado = datos.findIndex((operacion) => operacion.id == idOperacion)
 
+  datos.splice(indiceBuscado, 1, {...nuevosDatos, id: idOperacion});
+
+  guardarEnLS("operaciones", datos)
+
+  return datos
+}
+
+// eventos editar y eliminar
 function agregarEventosEditYDelete() {
-  const $$arrayButtonsDelete = $$(".button-delete")
-  const $$arrayButtonsEdit = $$(".button-edit")
+  let $$arrayButtonsDelete = $$(".button-delete")
+  let $$arrayButtonsEdit = $$(".button-edit")
 
   $$arrayButtonsDelete.forEach(button => {
     button.addEventListener("click", (e) => {
-      console.log(e.target.id)
       const nuevoArray = quitarOperacion(e.target.id)
       pintarOperaciones(nuevoArray)
+      hayOp()
+      actualizarBalance()
     })
   })
 
-///////
+//////
 
-let $descripcionEditar = $("descripcion-editar");
-let $montoEditar = $("monto-editar");
-let $selectTipoEditar = $("tipo-editar");
-let $selectCategoriaEditar = $("categoria-editar");
-let inputFechaEditar = $("fecha-editar");
-let $botonEditar = $("boton-editar");
-let $formEditar = $("form-editar");
+// capturas de elementos input del formulario editar
+  let $descripcionEditar = $("#descripcion-editar");
+  let $montoEditar = $("#monto-editar");
+  let $selectTipoEditar = $("#tipo-editar");
+  let $selectCategoriaEditar = $("#categoria-editar");
+  let $inputFechaEditar = $("#fecha-editar");
 
 
   $$arrayButtonsEdit.forEach(button => {
     button.addEventListener("click", (e) => {
-      ocultarElemento([$sectionVistaBalance, $formAgregar ])
-      mostrarElemento([$formEditar])
+      ocultarElemento([$sectionVistaBalance, $formAgregar])
+      mostrarElemento([$sectionEditarOp])
 
-      const datos = leerLS("operaciones")
-      const opBuscada = datos.find(elem => elem.id === e.target.id)
+      const datos = leerLS("operaciones");
+      const opBuscada = datos.find(elem => elem.id === e.target.id);
 
-      $descripcionEditar.value = opBuscada.value
+      $descripcionEditar.value = opBuscada.descripcion;
+      $montoEditar.value = opBuscada.monto;
+      $selectTipoEditar.value = opBuscada.tipo;
+      $selectCategoriaEditar.value = opBuscada.categoria;
+      $inputFechaEditar.value = opBuscada.fecha;
 
       $formEditar.id = opBuscada.id
     })
   })
 }
 
-///////
+//subir los cambios del formulario editar
+$formEditar.addEventListener("submit", (evento) => {
+  evento.preventDefault();
 
-//agregar y volver a "vistaBalance"
-$botonAgregar.addEventListener("click", () => {
+  const datos = leerLS("operaciones")
+  const opBuscada = datos.find(elem => elem.id === evento.target.id)
+
+  const nuevosDatos = { 
+    descripcion: evento.target[0].value,
+    monto: Number(evento.target[1].value),
+    tipo: evento.target[2].value,
+    categoria: evento.target[3].value,
+    fecha: dayjs(evento.target[4].value).format("YYYY-MM-DD")
+  };
+  const datosModificados = editarOperacion(opBuscada.id, nuevosDatos)
+
+  pintarOperaciones(datosModificados);
+  actualizarBalance()
+  
+});
+
+//agregar edicion y volver a vista balance
+let $btnEditarOp = $("#boton-editar")
+
+$btnEditarOp.addEventListener("click", () => {
   mostrarElemento([$sectionVistaBalance, $vistaOpActivas]);
-  ocultarElemento([$sectionNuevaOp, $cardSinOp])
+  ocultarElemento([$sectionEditarOp, $cardSinOp])
+})
+
+// ===== card balance =======================
+
+//captura spans para mostrar montos
+let $totalGanancias = $("#total-ganancia");
+let $totalGastos = $("#total-gastos");
+let $montoTotal = $("#monto-total")
+
+
+//mostrar ganancias, gastos y total
+function actualizarBalance() {
+  const datos = leerLS("operaciones") 
+
+  const gananciasFiltradas = datos.filter(elemento => elemento.tipo === "ganancia")
+  let ganancias = gananciasFiltradas.reduce((acc, operacion) => acc + operacion.monto, 0)
+  $totalGanancias.innerText = `+$${ganancias}`
+  
+
+//////
+
+  const gastosFiltrados = datos.filter(elemento => elemento.tipo === "gasto")
+  let gastos = gastosFiltrados.reduce((acc, operacion) => acc + operacion.monto, 0)
+  $totalGastos.innerText = `-$${gastos}`
+
+/////
+
+  let total = ganancias - gastos
+  if(total < 0) {
+    $montoTotal.classList.add("text-red-500")
+    $montoTotal.innerText = `-$${total}`
+  } else if(total > 0) {
+    $montoTotal.classList.add("text-green-500")
+    $montoTotal.innerText = `+$${total}`
+  } else {
+    $montoTotal.classList.add("text-stone-700")
+    $montoTotal.innerText = `$${total}`
+  }
+
+}
+
+
+// ===== filtros =======================
+//captura de elementos HTML
+let $btnOcultarFiltros = $("#ocultar-filtros");
+let $selectTipo = $("#tipo");
+let $selectFiltroCategoria = $("#categoria");
+let $inputDesdeFecha = $("#desde-fecha");
+let $selectOrden = $("#orden");
+
+
+// $selectFilterType.addEventListener("input", (e) => {
+//   const datos = obtenerDatos("ventas")
+//   if(e.target.value !== "all") {
+//     const ventasFiltradas = datos.filter(elem => elem.type === e.target.value)
+//     pintarDatos(ventasFiltradas)
+//   } else {
+//     pintarDatos(datos)
+//   }
+// })
+
+
+//ocultar filtros
+
+//filtrar por tipo
+
+//filtrar por categoria
+
+//desde (fecha)
+$inputDesdeFecha.addEventListener("input", (e) => {
+  let datos = leerLS("operaciones");
+
+  let arrayFiltrado = datos.filter(operacion => new Date(operacion.fecha) >= new Date(e.target.value))
+  pintarOperaciones(arrayFiltrado)
+})
+
+//ordenar por
+$selectOrden.addEventListener("input", () => {
+  let datos = leerLS("operaciones");
+  if($selectOrden.value === "mas-reciente") {
+    let arrayOrdenado = datos.sort((a, b) => new Date(b.fecha) - new Date(a.fecha));
+    pintarOperaciones(arrayOrdenado);
+  } else if($selectOrden.value === "menos-reciente") {
+    let arrayOrdenado = datos.sort((a, b) => new Date(a.fecha) - new Date(b.fecha));
+    pintarOperaciones(arrayOrdenado);
+  } else if($selectOrden.value === "mayor-monto") {
+    let arrayOrdenado = datos.sort((a, b) => b.monto - a.monto);
+    pintarOperaciones(arrayOrdenado);
+  } else if($selectOrden.value === "menor-monto") {
+    let arrayOrdenado = datos.sort((a, b) => a.monto - b.monto);
+    pintarOperaciones(arrayOrdenado);
+  } else if($selectOrden.value === "a-to-z") {
+    let arrayOrdenado = datos.sort((a, b) => a.descripcion.localeCompare(b.descripcion))
+    pintarOperaciones(arrayOrdenado);
+  } else if($selectOrden.value === "z-to-a") {
+    let arrayOrdenado = datos.sort((a, b) => b.descripcion.localeCompare(a.descripcion))
+    pintarOperaciones(arrayOrdenado);
+  }
 })
 
 
-// function pintarDatos(array) {
-
-//   $divListVentas.innerHTML = "";
-//   for (const venta of array) {
-//     $divListVentas.innerHTML += `<div class="w-full border border-black my-2">
-//       <h5>Tipo: ${venta.type}</h5>
-//       <p>Valor: ${venta.value}</p>
-//       <p>Valor: ${dayjs(venta.date).format("DD/MM/YYYY")}</p>
-//       <p>Cantidad: ${venta.quantity}</p>
-//       <div>
-//         <button id="${venta.id}" class="button-edit border border-black shadow bg-green-600">Editar</button>
-//         <button id="${venta.id}" class="button-delete border border-black shadow bg-red-600">Eliminar</button>
-//       </div>
-//     </div>`
-//   }
-
-//   agregarEventosEditYDelete()
-// }
-
-
-// function agregarVenta(objetoNuevaVenta) {
-//   const datos = obtenerDatos("ventas")
-//   guardarDatos("ventas", [...datos, objetoNuevaVenta])
-// }
 
 
 
-// $formCreate.addEventListener("submit", (evento) => {
-//   evento.preventDefault();
-
-//   const nuevaVenta = {
-//     id: crypto.randomUUID(),
-//     type: evento.target[0].value,
-//     value: Number(evento.target[1].value),
-//     date: dayjs(evento.target[2].value).format("YYYY-MM-DD"),
-//     quantity: Number(evento.target[3].value)
-//   }
 
 
 
-//   funciones.agregarVenta(nuevaVenta)
-
-//   const datos = funciones.obtenerDatos("ventas")
-//   pintarDatos(datos)
-// })
+////////////
 
 
 
@@ -259,27 +385,6 @@ $botonAgregar.addEventListener("click", () => {
 
 
 
-// // boton agregar operacion
-// let $btnAgregar = $("#boton-agregar")
-
-// $btnAgregar.addEventListener("click", () => {
-//   mostrarElemento([$sectionVistaBalance, $vistaOpActivas])
-//   ocultarElemento([$cardSinOp, $sectionNuevaOp])
-// })
-
-// function agregarVenta(objetoNuevaVenta) {
-//   const datos = obtenerDatos("ventas")
-//   guardarDatos("ventas", [...datos, objetoNuevaVenta])
-// }
-
-// function obtenerDatos(key) {
-//   const datos = JSON.parse(localStorage.getItem(key))
-//   return datos ? datos : [];
-// }
-
-// function guardarDatos(key, data) {
-//   localStorage.setItem(key, JSON.stringify(data))
-// }
 
 
 
@@ -288,67 +393,107 @@ $botonAgregar.addEventListener("click", () => {
 
 
 
-// $("#button-view-home").addEventListener("click", () => {
-//   showElement([$sectionViewHome])
-//   hideElement([$sectionViewVenta, $sectionViewReporte])
-// })
-
-// cuando hago click en el boton nueva operacion pasan las siguientes cosas
-// + se abre la vista nueva operacion con el formulario para rellenar la operacion
-// + se oculta la vista balance o la vista operaciones activas
-
-
-
-
-
-
-
-
-
-// CLASES 
-
-// $modoOscuro.addEventListener("click", () => {
-//     if($body.classList.contains("modo-claro")) {
-//         $body.classList.remove("modo-claro")
-//         $body.classList.add("modo-oscuro");
-//         $modoOscuro.style.display = "none";
-//         $modoClaro.style.display = "flex"
-//     }
-// })
-
+//==== VISTA CATEGORIAS =================================
+/////
+//captura del elemento contenedor de categorias
 let $ulCategorias = $("#lista-categorias")
 
-function pintarCategorias(arrayCategorias) {
+//mostrar las categorias existentes
+function pintarCategorias() {
+  let todasLasCat = leerLS("categorias")
+
   $ulCategorias.innerHTML = "";
-  for (const { id, nombre} of arrayCategorias) {
+
+  for (const {id, nombre} of todasLasCat) {
     $ulCategorias.innerHTML += `<li>
     <span> ${nombre}</span>
+    <div>
+      <button id="${id}" class="button-edit border border-black shadow bg-green-600">Editar</button>
+      <button id="${id}" class="button-delete border border-black shadow bg-red-600">Eliminar</button>
+    </div>
     </li>`
   }
+  guardarEnLS("categorias", todasLasCat)
 }
 
-// function pintarDatos(arrayPersonajes) {
-//   $main.innerHTML = "";
-//   for (const { image, name, species, house, gender, alive, alternate_names } of arrayPersonajes) {
-//     $main.innerHTML += `<div class="flex flex-col justify-center items-center border border-2 border-black rounded-xl w-5/6 md:w-5/12 lg:w-3/12 mx-8 my-2 h-72">
-//       <img src="${image}" class="h-24 w-24">
-//       <p>Nombre: ${capitalize(name)}</p>
-//       <div class="flex flex-row justify-around w-full flex-wrap">
-//         <p class="w-2/5">Especie: ${capitalize(species)}</p>
-//         <p class="w-2/5">Casa: ${capitalize(house)}</p>
-//         <p class="w-2/5">Genero: ${capitalize(gender)}</p>
-//         <p class="w-2/5">Vivo: ${alive ? "Si, esta vivito" : "No, se despidio"}</p>
-//       </div>
-//       <p>Nombres Alt: ${alternate_names.length > 0 ? alternate_names : "No tiene nombres alternativos"}</p>
-//     </div>`
+//===== agregar categoria ==================
+//captura de elementos HTML
+let $inputCategoria = $("#input-categoria");
+let $btnAgregarCategoria = $("#agregar-categoria");
+/////
+
+//evento click del boton agregarCategoria
+$btnAgregarCategoria.addEventListener("click", () => {
+
+  let todasLasCat = leerLS("categorias")
+
+  const nuevaCategoria = {
+    id: crypto.randomUUID(),
+    nombre: $inputCategoria.value
+  }
+
+  todasLasCat.push(nuevaCategoria)
+  guardarEnLS("categorias", todasLasCat)
+  pintarCategorias(todasLasCat)
+  $inputCategoria.value = ""
+  actualizarCategorias()
+
+})
+
+function actualizarCategorias() {
+  leerLS("categorias")
+  //agregar las nuevas y viejas categorias en el select de nuevaOp y editarOp
+  //quitar las eliminadas cambiar el nombre de las editadas
+  //eliminar las operaciones que esten subidas con la categorias recien eliminada
+}
+
+//tienen que actualizarse las categorias en el select, puede hacerse por fuera con una funcion y ejecutarla en este click, puede reutilizarse para cuando elimine o edite alguna categoria
+
+// + editar categoria
+//      - actualizar el localstorage con el nombre nuevo, esto probablemente haga que todas cambien de valor
+//      - cambiar el nombre de la  categoria en la operacion
+//      - actualizar el select
+
+// + eliminar categoria
+//      - actualizar el localstorage con la categoria eliminada
+//      - eliminar las operaciones que estaban en esa categoria y volver a pintarOperaciones()
+//      - actualizar el select
+
+
+
+
+
+// $formAgregar.addEventListener("submit", (evento) => {
+//   evento.preventDefault();
+
+//   const nuevaOp = {
+//     id: crypto.randomUUID(),
+//     descripcion: evento.target[0].value,
+//     monto: Number(evento.target[1].value),
+//     tipo: evento.target[2].value,
+//     categoria: evento.target[3].value,
+//     fecha: dayjs(evento.target[4].value).format("YYYY-MM-DD")
 //   }
-// }
+
+//   todasLasOp.push(nuevaOp)
+//   guardarEnLS("operaciones", todasLasOp)
+//   pintarOperaciones(todasLasOp)
+//   hayOp()
+//   actualizarBalance()
+   
+// })
 
 
 
+//////////////////
 
+
+
+////////////
 
 window.onload = () => {
   todasLasOp = leerLS("operaciones")
   pintarOperaciones(todasLasOp)
+  hayOp()
+  actualizarBalance()
 }
